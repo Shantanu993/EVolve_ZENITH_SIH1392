5d623b0f19090f96d5cf0cb4616060cabef05f88
Shantanu993 <shantanugahtori4@gmail.com>
2023-09-13 03:20:01 +0530

    modified code

--- components/Pricing.js
+++ components/Pricing.js
@@ -222,10 +222,6 @@ const Pricing = () => {
                 </div>
                 <ButtonPrimary>Get Started</ButtonPrimary>
               </div>
-              <div
-                className="absolute bg-black-600 opacity-5 w-11/12 rounded-lg h-60 sm:h-56 top-0 mt-8 mx-auto left-0 right-0"
-                style={{ filter: "blur(114px)" }}
-              ></div>
             </motion.div>
           </ScrollAnimationWrapper>
         </div>
