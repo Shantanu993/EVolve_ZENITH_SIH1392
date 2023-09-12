bc3faba0ad5fe92be2e93acf1f3bb9b29fa4e589
Shantanu993 <shantanugahtori4@gmail.com>
2023-09-12 19:11:24 +0530

    added find facility page

--- pages/nearby.js
+++ pages/nearby.js
@@ -1,11 +1,19 @@
 import Header from "@/components/Layout/Header.js";
 import Footer from "@/components/Layout/Footer.js";
+import ButtonPrimary from "@/components/misc/ButtonPrimary";
 
 const nearby = () => {
   return (
     <>
       <Header />
-      <div className="h-[100vh]">nearby</div>;
+      <div className="bg-teagreen-500">
+        <div className="max-w-screen-xl pt-[9rem] px-4 xl:px-[1rem] ml-4 h-[100vh]">
+          <ButtonPrimary>
+            <a href="/nearby">Locate Nearby Facility</a>
+          </ButtonPrimary>
+        </div>
+      </div>
+      ;
       <Footer />
     </>
   );
