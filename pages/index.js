e3c8453c9e3f224d22abc5390f44f994ee122402
Shantanu993 <shantanugahtori4@gmail.com>
2024-03-02 11:02:26 +0530

    changed title

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import SeoHead from "../components/SeoHead";
 export default function Home() {
   return (
     <>
-      <SeoHead title="E-Waste Collector" />
+      <SeoHead title="E-Volve_Mavericks" />
       <Layout>
         <Hero />
         <Feature />
